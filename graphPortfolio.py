bff762f15c0adf24a2b8b044aab62e55c3ec08e5
Cyrus Farsoudi <cfarsoudi@hotmail.com>
2021-04-30 17:06:52 -0700

    Took out hard coded PYTHONPATH

--- graphPortfolio.py
+++ graphPortfolio.py
@@ -1,7 +1,6 @@
 #!/Library/Frameworks/Python.framework/Versions/3.9/bin/python3
 
 import sys, matplotlib.pyplot as plt
-sys.path.insert(0,'/usr/local/lib/python3.7/site-packages/')
 
 def readData(fileName):
     f = open(fileName, "r")
